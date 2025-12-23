c85706793d41ad2480b06bb9018357c705d9bc17
Leanna Bucio <a02440198@usu.edu>
2025-12-22 20:20:25 -0700

    implemented usage and imported sys

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 # Will later add sys for user control
-import json
+import json, sys
 
+from usage import usage
 from get_data import get_data
 from clean import clean_data, fix_times
 
 def main():
+
+    if len(sys.argv) < 2:
+        usage()
+
     raw_data = get_data() # will later add sys as arguments
     cleaned_data = clean_data(raw_data) # second arg can be sys later
     fix_times(cleaned_data)
