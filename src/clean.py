77d234a18bb92b54b4a962c0f920b52e8c6a2535
Leanna Bucio <a02440198@usu.edu>
2025-12-23 20:22:53 -0700

    updated clean data to match user manual specifications

--- src/clean.py
+++ src/clean.py
@@ -9,7 +9,11 @@ def clean_data(raw_data, info_keep=default_keeps):
 
   return clean_data
 
-def fix_times(data, chosen_variables=["sunrise", "sunset"]):                                                                     
-    for var in chosen_variables:                                                                                                 
-        if var in data["daily"]: # check if the daily variable is either sunset sunrise, ignore otherwise                        
+"""---------"""
+
+def fix_times(data, chosen_variables="sunrise,sunset"):
+    for var in chosen_variables.split(","):
+        if var in data["daily"]: # will need to make daily a variable later to implement forecast options
+
+            # This goes through all of the date-times in the data and cleans it up to just times
             data["daily"][var] = [t.split("T")[1] for t in data["daily"][var]]                                                   
