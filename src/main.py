c974614f32bf0b557c0ed941a72637ad1ab45362
Leanna Bucio <a02440198@usu.edu>
2025-12-23 20:26:11 -0700

    implemented plan to implement user control over forecast specifications and variables

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# Will later add sys for user control
 import json, sys
 
 from usage import usage
@@ -10,12 +9,22 @@ def main():
     if len(sys.argv) < 2:
         usage()
 
-    raw_data = get_data() # will later add sys as arguments
-    cleaned_data = clean_data(raw_data) # second arg can be sys later
+    if sys.argv[1] != "daily":
+        usage("Not a valid forecast specifier") # make sure other default works
+
+    if len(sys.argv) > 2:
+        raw_data = get_data(sys.argv[2]) # under assumption that the other defaults will still work
+        cleaned_data = clean_data(raw_data, ["latitude","longitude",sys.argv[1]])
+        fix_times(cleaned_data, sys.argv[2])
+        return json.dumps(cleaned_data, indent=1)
+
+    # System default output
+    raw_data = get_data()
+    cleaned_data = clean_data(raw_data)
     fix_times(cleaned_data)
   
     return json.dumps(cleaned_data, indent=1)
-    # finished product, ready for use :)
+
 
 week_sun_times = main()
 print(week_sun_times)
