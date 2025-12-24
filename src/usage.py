a2095a8999c078ce37ece143947cde8137d278fe
Leanna Bucio <a02440198@usu.edu>
2025-12-23 20:21:30 -0700

    updated user manual tips

--- src/usage.py
+++ src/usage.py
@@ -2,17 +2,19 @@ import sys
 
 hourly = """main.py hourly [-v VARIABLES]
         Presents weather information by the hour of the next 7 days
-        -v  Specify what information to display as variables (default VARIABLES=precipitation_percentage)"""
+        -v  Specify what information to display as variables (default 
+        VARIABLES=precipitation_percentage) all seperated by commas only"""
 
 daily = """main.py daily [-v VARIABLES]
         Presents weather information by the day for the next 7 days
-        -v  Specify what information to display as variables (default VARIABLES=sunrise sunset)"""
+        -v  Specify what information to display as variables (default VARIABLES=sunrise,sunset)
+        all seperated by commans only"""
 
 def usage(error=None, tool=None):
 
     # print any error message as requested
     if error is not None:
-        print(f"Error: {error}\n", file-sys.stderr)
+        print(f"Error: {error}\n", file=sys.stderr)
 
     if tool == "hourly":
         print(f"{hourly}", file=sys.stderr)
